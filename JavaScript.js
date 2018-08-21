0ce9fa47726f0e195100c66cf6e490d673a8d45f
Nik <nik101skryto@gmail.com>
2018-08-21 21:34:38 +0400

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,11 +1,11 @@
 function add_img() {
     var field = document.getElementById('field').value;
-    var block = document.createElement('div');
+    var block = document.createElement('td');
     block.id = 'workimage';
     var image = document.createElement('img');
     image.src = field;
     block.appendChild(image);
-    var obj = document.getElementById('img_container');
+    var obj = document.getElementById('img_container_2');
     obj.insertBefore(block, obj.children[0]);
 }
 
